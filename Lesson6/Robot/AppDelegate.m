af9ef64ae86b0a10809a5af805adb5d6d9351608
Dinur Balov <119072459+dinur0@users.noreply.github.com>
2024-04-04 16:48:22 +0300

    Update AppDelegate.m

--- Lesson6/Robot/AppDelegate.m
+++ Lesson6/Robot/AppDelegate.m
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.m
-//  Lesson6
-//
-//  Created by хех on 21.02.2024.
-//
-
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
@@ -15,7 +8,6 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
     return YES;
 }
 
@@ -24,17 +16,12 @@
 
 
 - (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
     return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
 }
 
 
 - (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-}
+ }
 
 
 @end
